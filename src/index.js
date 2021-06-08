0b073cf96c78417ee60747564b4eb5d8d057089c
mrack69 <klyushentsev@gmail.com>
2021-06-08 21:16:11 +0300

    feat: add func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 > 0) {
+    return false;
+  }
+  let length = str.length;
+  let count = 0;
+  for(let j = 0; j < length / 2; j++) {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      str = str.replace(bracketsConfig[i].join(''), '');
+    }
+    count++;
+  }
+  if(str.length > 0) return false
+  else return true
 }
